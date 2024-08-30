a940a4169f7bb6a661e13e89ed62e685adb25d62
Atair Hubler Junior <atairjrbr@gmail.com>
2024-08-30 08:57:17 -0300

    Criado novo projeto chamado catalogo de jogos Adicionado classes a este projeto

---
+++ CatalogoDeJogos/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+/*
+ 
+Desenvolver uma classe que representa um catálogo de jogos, com uma lista de Jogos e métodos para manipular essa lista, bem como um construtor que faça sua inicialização.
+
+
+Classes:
+
+Jogo
+    Contrutor
+
+Catalogo
+    Metodo adicionar jogo
+    Metodo remover jogo
+    Metodo mostrar lista de jogos
+
+
+
+*/
